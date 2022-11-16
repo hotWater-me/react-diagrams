2d535deece2dd6c4edc0cd3a201f401762ec1ba9
wangyp <wb-wyp737365@antgroup.com>
2022-11-16 23:35:31 +0800

    chore: 改造打包 以便dedebug

--- diagrams-demo-gallery/.storybook/main.js
+++ diagrams-demo-gallery/.storybook/main.js
@@ -3,5 +3,27 @@ module.exports = {
 	core: {
 		builder: 'webpack5'
 	},
-	addons: ['@storybook/addon-actions']
+	addons: ['@storybook/addon-actions'],
+	webpackFinal: async (config, { configType }) => {
+		// Make whatever fine-grained changes you need
+		// Return the altered config
+		config.resolve.alias = {
+			...config.resolve.alias,
+			'@projectstorm/react-canvas-core':
+				'/Users/yunpeng/Desktop/openSource/react-diagrams/packages/react-canvas-core',
+
+			'@projectstorm/react-diagrams': '/Users/yunpeng/Desktop/openSource/react-diagrams/packages/react-diagrams',
+
+			'@projectstorm/react-diagrams-core':
+				'/Users/yunpeng/Desktop/openSource/react-diagrams/packages/react-diagrams-core',
+
+			'@projectstorm/react-diagrams-defaults':
+				'/Users/yunpeng/Desktop/openSource/react-diagrams/packages/react-diagrams-defaults',
+
+			'@projectstorm/react-diagrams-routing':
+				'/Users/yunpeng/Desktop/openSource/react-diagrams/packages/react-diagrams-routing'
+		};
+		console.log(config);
+		return config;
+	}
 };
