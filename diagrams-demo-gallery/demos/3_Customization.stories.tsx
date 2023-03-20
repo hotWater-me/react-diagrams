8d0a719649eada9228b02f9e3a8506487b8285b2
wangyp <wb-wyp737365@antgroup.com>
2023-03-20 18:46:35 +0800

    feat: 快捷键开发

--- diagrams-demo-gallery/demos/3_Customization.stories.tsx
+++ diagrams-demo-gallery/demos/3_Customization.stories.tsx
@@ -10,9 +10,11 @@ import demo_custom_action from './demo-custom-action';
 import demo_cust_nodes from './demo-custom-node1';
 import demo_cust_links from './demo-custom-link1';
 import demo_cust_links2 from './demo-custom-link2';
+import demo_custom_shortcut from './demo-shortcut'
 
 export const CustomDiamondNode = demo_cust_nodes;
 export const CustomAnimatedLinks = demo_cust_links;
 export const CustomLinkEndsWithArrows = demo_cust_links2;
 export const CustomLinkLabel = demo_custom_link_label;
 export const CustomEvent = demo_custom_action;
+export const CustomShortcut = demo_custom_shortcut;
